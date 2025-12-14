1d5fbdcad9e1ffa2a9e6c372121f0dc3277c49e9
Your Real Name <surafelmuhabaw17@gmail.com>
2025-12-14 22:52:32 +0300

    update

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -9,12 +9,12 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
   supabase: {
     redirect: false,
-    url: process.env.SUPABASE_URL || 'https://hnruyktmlqskqqipjcrc.supabase.co',
-    key: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhucnV5a3RtbHFza3FxaXBqY3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzODkwODksImV4cCI6MjA4MDk2NTA4OX0.LZGoNmAE6qjksg2ugEnVHUdWpNX3k2pngrQ-x-6QZSk',
+    url: 'https://hnruyktmlqskqqipjcrc.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhucnV5a3RtbHFza3FxaXBqY3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzODkwODksImV4cCI6MjA4MDk2NTA4OX0.LZGoNmAE6qjksg2ugEnVHUdWpNX3k2pngrQ-x-6QZSk',
   },
   runtimeConfig: {
     public: {
-      backendUrl: process.env.NUXT_PUBLIC_BACKEND_URL || 'https://e-learning-1-qvk8.onrender.com',
+      backendUrl: 'https://e-learning-1-qvk8.onrender.com',
     }
   },
   compatibilityDate: '2024-01-01',
