717e25cc9b836216fddec2899c62b892252f0c0f
Your Real Name <surafelmuhabaw17@gmail.com>
2025-12-14 21:38:20 +0300

    some

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -12,9 +12,9 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      backendUrl: process.env.NUXT_PUBLIC_BACKEND_URL || 'http://localhost:5000',
-      NUXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL,
-      NUXT_PUBLIC_SUPABASE_ANON_KEY: process.env.SUPABASE_KEY,
+      backendUrl: process.env.NUXT_PUBLIC_BACKEND_URL || 'https://e-learning-1-qvk8.onrender.com',
+      NUXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL || 'https://hnruyktmlqskqqipjcrc.supabase.co',
+      NUXT_PUBLIC_SUPABASE_ANON_KEY: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhucnV5a3RtbHFza3FxaXBqY3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzODkwODksImV4cCI6MjA4MDk2NTA4OX0.LZGoNmAE6qjksg2ugEnVHUdWpNX3k2pngrQ-x-6QZSk',
     }
   },
   compatibilityDate: '2024-01-01',
