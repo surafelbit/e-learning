e52564e9a7d56bb3f5b5c5d2f763cd892fc207ad
Your Real Name <surafelmuhabaw17@gmail.com>
2025-12-14 22:22:57 +0300

    cnages

---
+++ frontend/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: [
+    function (to, from) {
+      return navigateTo('/login')
+    }
+  ]
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <p>Redirecting to login...</p>
+  </div>
+</template>
