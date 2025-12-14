0cb90bb156687c2c790e2f2ff160e098c98e129e
Your Real Name <surafelmuhabaw17@gmail.com>
2025-12-14 22:09:39 +0300

    cnages

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -8,13 +8,13 @@ const __dirname = path.dirname(__filename);
 export default defineNuxtConfig({
   modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
   supabase: {
-    redirect: false, // We will handle redirection manually in middleware
+    redirect: false,
+    url: process.env.SUPABASE_URL || 'https://hnruyktmlqskqqipjcrc.supabase.co',
+    key: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhucnV5a3RtbHFza3FxaXBqY3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzODkwODksImV4cCI6MjA4MDk2NTA4OX0.LZGoNmAE6qjksg2ugEnVHUdWpNX3k2pngrQ-x-6QZSk',
   },
   runtimeConfig: {
     public: {
       backendUrl: process.env.NUXT_PUBLIC_BACKEND_URL || 'https://e-learning-1-qvk8.onrender.com',
-      NUXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL || 'https://hnruyktmlqskqqipjcrc.supabase.co',
-      NUXT_PUBLIC_SUPABASE_ANON_KEY: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhucnV5a3RtbHFza3FxaXBqY3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzODkwODksImV4cCI6MjA4MDk2NTA4OX0.LZGoNmAE6qjksg2ugEnVHUdWpNX3k2pngrQ-x-6QZSk',
     }
   },
   compatibilityDate: '2024-01-01',
